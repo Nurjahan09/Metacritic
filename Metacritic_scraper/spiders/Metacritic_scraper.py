f24a1c538c5b35c120b976724c11a0c6ccd83f8d
Nurjahan09 <nurjahan2009@gmail.com>
2015-06-12 16:28:27 +0200

    first commit

---
+++ Metacritic_scraper/spiders/Metacritic_scraper.py
@@ -0,0 +1,59 @@
+from scrapy.selector import HtmlXPathSelector
+from scrapy.contrib.spiders import CrawlSpider
+from scrapy.log import *
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors import LinkExtractor
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+import scrapy
+from scrapy.http import Request
+import urlparse
+from scrapy.item import Item, Field
+from scrapy import Spider
+from scrapy.selector import Selector
+ 
+class MetacriticItem(scrapy.Item):
+    Album_title = scrapy.Field()
+    Artist = scrapy.Field()
+    Release_date = scrapy.Field()
+    Critic_review = scrapy.Field()
+    User_review = scrapy.Field()
+    Score = scrapy.Field()
+ 
+class MetaSpider(CrawlSpider):
+ 
+    name = 'Metacritic_scraper'
+    start_urls = [
+        'http://www.metacritic.com/browse/albums/release-date/available/date'
+    ]
+    rules = (
+    Rule(SgmlLinkExtractor(allow=('http://www.metacritic.com/browse/albums/release-date/available/date')), callback="parse", follow= True),
+
+)
+ 
+    def parse(self, response):
+       hxs = HtmlXPathSelector(response)
+       urls = hxs.select('//*[@id="main"]/div[1]/div[2]/div[2]/div/ol/li/div/div/a/@href').extract()  ## only grab url with content in url name
+       for i in urls:
+           yield Request(urlparse.urljoin("http://www.metacritic.com", i[1:]),callback=self.parse_url)
+
+       #ex_blog_urls = hxs.select('//*[@id="main"]/div/div[4]/div/div[1]/div[2]/ol/li[1]/div/div/div/div/div/div[2]/ul/li[2]/a/@href').extract()  ## only grab url with content in url name
+       #for j in ex_blog_urls:
+           #yield Request(urlparse.urljoin("http://www.metacritic.com/music", j[1:]),callback=self.parse_url)
+
+       next_page = hxs.select('//*[@id="main"]/div[1]/div[3]/div/div/div/div[1]/span[2]/a/@href').extract()  ## only grab url with content in url name
+       for i in next_page:
+           yield Request(urlparse.urljoin("http://www.metacritic.com", i[1:]),callback=self.parse)
+
+
+
+    def parse_url(self, response):
+       hxs = HtmlXPathSelector(response)
+       item = MetacriticItem()
+       item['Album_title'] = hxs.select('//div/div[1]/div[2]/span/a/span/text()').extract()
+       item['Artist'] = hxs.select('//*[@id="main"]/div/div[1]/div[3]/a/span/text()').extract()
+       item['Release_date'] = hxs.select('//*[@id="main"]/div/div[1]/div[4]/ul/li[2]/span[2]/text()').extract()
+       item['Critic_review'] = hxs.select('//div[@class="source"]/a/@href').extract() ## this grabs it
+       item['User_review'] = hxs.select('//div[@class="name"]/a/@href').extract() 
+       item['Score'] = hxs.select('//*[@id="main"]/div/div[4]/div/div[1]/div[2]/ol/li[1]/div/div/div/div/div/div[1]/div[1]/div[2]/div/text()').extract() 
+       return item
+       
